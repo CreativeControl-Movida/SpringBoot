545a63ecb5ae51a86586ca0d716bef5e13055f45
unknown <catharina.moral@gmail.com>
2020-08-30 21:33:34 -0300

    Estrtura Spring Initializr

---
+++ src/test/java/com/br/creativecontrol/movida/MovidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.creativecontrol.movida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
